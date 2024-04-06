5d266f73deb672c4ce892b1efe35b12fccebd51c
aditya-0212 <adityachoubisa02@gmail.com>
2024-04-06 20:56:42 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by APPLE on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
